1cf9552c662f4a5aa583ce8a98349cd4d33f2072
KRG0405 <2s01047904773@gmail.com>
2025-05-14 14:15:37 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title("✅ Group3 project")
 st.caption("This is a group project of G3")
 
-st.write("Creating contents")
+st.write("Creating contents...")
 st.markdown("---")
