7edf3e910fa210091ad71832c33a451efd43336b
KRG0405 <2s01047904773@gmail.com>
2025-05-14 15:16:35 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.write("This app is under construction.")
 
-url="https://github.com/MK316/streamlit25/raw/main/images/image00.png"
+url="images/KakaoTalk_20250514_142955603.jpg"
 st.image(url, caption="Image link", width=300)  # width in pixels
 
